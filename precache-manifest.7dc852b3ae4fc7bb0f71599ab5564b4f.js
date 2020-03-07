1563772dae91f51e25b9d2d54635110d995b7f2d
Surya Khandavilli <Khandavillisurya@gmail.com>
2020-03-07 23:38:39 +0530

    Updates

---
+++ precache-manifest.7dc852b3ae4fc7bb0f71599ab5564b4f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15ff78439c8966d23648f8a30a19ded",
+    "url": "/suryakh/suryakh.github.io.git/index.html"
+  },
+  {
+    "revision": "8a92410bcda7bfba0127",
+    "url": "/suryakh/suryakh.github.io.git/static/css/main.de4224b6.chunk.css"
+  },
+  {
+    "revision": "45cd0cf710cc3e873f6f",
+    "url": "/suryakh/suryakh.github.io.git/static/js/2.1ed7c1a2.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/suryakh/suryakh.github.io.git/static/js/2.1ed7c1a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a92410bcda7bfba0127",
+    "url": "/suryakh/suryakh.github.io.git/static/js/main.c382d33b.chunk.js"
+  },
+  {
+    "revision": "737f80b1eb818fdfc10a",
+    "url": "/suryakh/suryakh.github.io.git/static/js/runtime-main.5f259abd.js"
+  },
+  {
+    "revision": "5f732fc63f45a9bad86b35e19f25967e",
+    "url": "/suryakh/suryakh.github.io.git/static/media/Billgenerator.5f732fc6.png"
+  },
+  {
+    "revision": "eca89ab70bf202f75da64bd48e61e347",
+    "url": "/suryakh/suryakh.github.io.git/static/media/Gitcalender.eca89ab7.png"
+  },
+  {
+    "revision": "8f8e97a061cdfc5512f449cf85fab6a7",
+    "url": "/suryakh/suryakh.github.io.git/static/media/GoogleTimer.8f8e97a0.png"
+  },
+  {
+    "revision": "9920b2f97e3923821bc052b42f8bb81c",
+    "url": "/suryakh/suryakh.github.io.git/static/media/Marksrecorder.9920b2f9.png"
+  },
+  {
+    "revision": "cbb3bf7d1da0442e05877f5e3e2811f9",
+    "url": "/suryakh/suryakh.github.io.git/static/media/OMDB.cbb3bf7d.png"
+  },
+  {
+    "revision": "2c6b24c7eaafe7dfd8f3f8f46bd8bec7",
+    "url": "/suryakh/suryakh.github.io.git/static/media/Taskmanager.2c6b24c7.png"
+  },
+  {
+    "revision": "9c004659df8ba7cd05e6c98eaf50a0af",
+    "url": "/suryakh/suryakh.github.io.git/static/media/Todo.9c004659.png"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/suryakh/suryakh.github.io.git/static/media/bootstrapimg.71e42b5a.svg"
+  },
+  {
+    "revision": "d3ad67fa720fd7aee8c322caff911082",
+    "url": "/suryakh/suryakh.github.io.git/static/media/css-3.d3ad67fa.svg"
+  },
+  {
+    "revision": "318d58cbc47ace3e1baf4f035cab5b53",
+    "url": "/suryakh/suryakh.github.io.git/static/media/flask.318d58cb.svg"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/suryakh/suryakh.github.io.git/static/media/html-5.24845c5c.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/suryakh/suryakh.github.io.git/static/media/javascript.87711407.svg"
+  },
+  {
+    "revision": "8ec9bacf8bc78b8ad161b5e9a2a16e87",
+    "url": "/suryakh/suryakh.github.io.git/static/media/jquery.8ec9bacf.svg"
+  },
+  {
+    "revision": "ba99aecd075dddfb01b64b37724165fb",
+    "url": "/suryakh/suryakh.github.io.git/static/media/material-ui.ba99aecd.svg"
+  },
+  {
+    "revision": "00b27c6bbcdc5e3c924b155ef99e5b23",
+    "url": "/suryakh/suryakh.github.io.git/static/media/menu.00b27c6b.svg"
+  },
+  {
+    "revision": "dd2a5a358ea682bed45bf98bc842b3ed",
+    "url": "/suryakh/suryakh.github.io.git/static/media/mysql.dd2a5a35.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/suryakh/suryakh.github.io.git/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "206de3221cc8c10c70bef4508aa72b9d",
+    "url": "/suryakh/suryakh.github.io.git/static/media/web_developer.206de322.png"
+  }
+]);
